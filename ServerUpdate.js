e434e9bd4f5947e4c886f9c23ba5903b9273bb71
Thybris Vandenbroucke <thybrisvdb@outlook.com>
2020-04-23 09:39:42 +0200

    serverupdate.js

--- ServerUpdate.js
+++ ServerUpdate.js
@@ -6,3 +6,18 @@ function RequestAllLocaions(){
         console.log(data)
     })
 }
+var Username =document.getElementById()
+var Shop = document.getElementById()
+var City = document.getElementById()
+var Rating = document.getElementById()
+var Review = document.getElementById()
+document.getElementById("button").addEventListener("click", PushAllData);
+function PushAllData(){
+    document.getElementById("id")
+    var list = [];
+    list.push(Username)
+    list.push(Shop)
+    list.push(City)
+    list.push(Rating)
+    list.push(Review)
+}
